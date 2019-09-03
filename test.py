5f10484017b5692462010454c9157f5d80235469
Krissmedt <kristoffer@smedt.dk>
2019-09-03 15:59:05 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -30,67 +30,122 @@ def cell_index(pos,O,dh):
     
     return li
 
-limits = [-1,1]
-O = limits[0]
-dx_mag = 1
-qSum = -2
+def scatter_1d(pos_list,q):
+    for pii in range(0,nq):
+        li = cell_index(pos_list[pii,2],O,dh)
 
-res = 20
-ppc = 1
-q = qSum/(res*ppc)
+        rpos = pos_list[pii,2] - O - li*dh
+        h = rpos/dh
+        
+        meshq[li] += q*(1-h)
+        meshq[li+1] += q*(h)
+    return meshq
+
+outerLimits = [-3,3]
+limits = [-1,1]
+plot_ilims = [0,None]
+O = limits[0]
+dx_mag = 10
+qSum = -(limits[1]**3 - limits[0]**3)
+
+res = 200
+ppc = 1000
+
 nq = np.int(res*ppc)
 meshq = np.zeros((res+1))
 meshpos = np.linspace(limits[0],limits[1],res+1)
 
 L = limits[1] - limits[0]
-spacing = L/nq
+outerL = outerLimits[1] - outerLimits[0]
+spacing = outerL/nq
 
 x0 = [(i+0.5)*spacing for i in range(np.int(-nq/2),np.int(nq/2))]
-xi = [-dx_mag*np.power(x0i/L,3) for x0i in x0]
+xi = [-dx_mag*np.power(x0i/outerL,3) for x0i in x0]
 x2 = [-3*dx_mag*np.power(x0i,2) for x0i in x0]
 
-x = np.zeros(len(x0))
+x = []
 for i in range(0,nq):
-    x[i] = x0[i]+xi[i]
-    print(x0[i])
-    if x[i] >= limits[1]:
-        x[i] = limits[0] + (x[i]-limits[1])
-    elif x[i] < limits[0]:
-        x[i] = limits[1] - (limits[0]-x[i])
+    x.append(x0[i]+xi[i])
+    
+    if x[i] >= limits[1] or x[i] < limits[0]:
+        x[i] = 'out'
+        xi[i] = 'out'
+        x0[i] = 'out'
         
-pos_list = np.zeros((nq,3),dtype=np.float)
-y = np.zeros((nq,3),dtype=np.float)
-pos_list[:,2] = np.array(x)
-print(pos_list[:,2])
+        
+x = [y for y in x if y != 'out']
+x0 = [y for y in x0 if y != 'out']
+xi = [y for y in xi if y != 'out']
 
+nq = len(x)
+
+pos_spec1 = np.zeros((nq,3),dtype=np.float)
+pos_spec2 = np.zeros((nq,3),dtype=np.float)
+
+pos_spec1[:,2] = np.array(x)
+pos_spec2[:,2] = np.array(x0)
+
+q = qSum/nq
+q2 = -q
 
 dh = L/res
-for pii in range(0,nq):
-    li = cell_index(pos_list[pii,2],O,dh)
-    rpos = pos_list[pii,2] - O - li*dh
-    h = rpos/dh
-    
-    meshq[li] += q*(1-h)
-    meshq[li+1] += q*(h)
 
-#meshq[0] += meshq[-1]
-#meshq[-1] = meshq[0]
+meshq = scatter_1d(pos_spec1,q)
+#meshq = scatter_1d(pos_spec2,q2)
 
-spacing = []
-for pii in range(1,nq):
-    spacing.append(abs(pos_list[pii,2]-pos_list[pii-1,2]))
-
-print(np.sum(meshq))
+meshq[0] = meshq[0]*2
+meshq[-1] = meshq[-1]*2
+#meshq = meshq + nq*q2/(res+1)
 meshrho = meshq/dh
 
+
+spacing = []
+p_midpoints = []
+for pii in range(1,nq):
+    space = abs(pos_spec1[pii,2]-pos_spec1[pii-1,2])
+    spacing.append(space)
+    p_midpoints.append(x[pii]-space)
+
+
+si = plot_ilims[0]
+ei = plot_ilims[1]
+
 fig1 = plt.figure(1)
-plt.plot(x0,x2,'bo')
+plt.plot(p_midpoints,spacing,'bo')
 
 fig2 = plt.figure(2)
-plt.plot(meshpos[1:-1],meshq[1:-1])
+plt.plot(x0,xi,'b')
 
-fig3 = plt.figure(3)
-plt.plot(meshpos[1:-1],meshrho[1:-1])
 
-fig4 = plt.figure(4)
-plt.plot(meshpos[1:-1],-3*np.power(meshpos[1:-1],2))
+fig3, ax3 = plt.subplots()
+ax3.plot(meshpos[si:ei],meshq[si:ei],label=r'Mesh $q$')
+ax3.set_xlim([-1,1])
+#ax4.set_ylim([-0.21,0.21])
+
+ax3b = ax3.twinx()
+ax3.plot(meshpos[si:ei],-3*np.power(meshpos[si:ei],2)*dh,'r',label=r'Real $q$')
+#ax4b.set_ylim([-1,1])
+fig3.legend(loc='upper right',bbox_to_anchor=(0.85,0.83))
+
+
+fig4, ax4 = plt.subplots()
+ax4.plot(meshpos[si:ei],meshrho[si:ei],label=r'Mesh $\rho$')
+ax4.set_xlim([-1,1])
+#ax4.set_ylim([-0.21,0.21])
+
+ax4b = ax4.twinx()
+ax4.plot(meshpos[si:ei],-3*np.power(meshpos[si:ei],2),'r',label=r'Real $\rho$')
+#ax4b.set_ylim([-1,1])
+fig4.legend(loc='upper right',bbox_to_anchor=(0.85,0.83))
+
+print("")
+
+print("Total charge:")
+print("Particles: " + str(nq*q))
+print("Mesh: " + str(np.sum(meshq)))
+
+print("")
+
+print("Average density:")
+print("Particles: " + str(nq*q/L))
+print("Mesh: " + str(np.sum(meshrho)/(len(meshrho))))
