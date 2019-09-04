f4ff93af1244038f47482d0f25ce16bc1a1f578c
Krissmedt <kristoffer@smedt.dk>
2019-09-04 17:06:35 +0100

    Add files via upload

--- run_WinkelPenning.py
+++ run_WinkelPenning.py
@@ -3,75 +3,89 @@ from math import sqrt, fsum, pi, exp, cos, sin, floor
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
+from dataHandler2 import dataHandler2 as DH
 
 
 simulate = True
 sim_no = 0
 
+schemes = {'lobatto':'boris_SDC','boris':'boris_synced'}
 
-#schemes = {'lobatto':'boris_SDC','legendre':'boris_SDC','boris':'boris_synced'}
-schemes = {'lobatto':'boris_SDC'}
-
-M = 3
-iterations = [1,2,4,8,16]
-          
-omegaB = 25.0
-omegaE = 4.9
-epsilon = -1
-
-dt = np.linspace(10**-1,10,10)
-dt = dt/omegaB
-dt = np.flip(dt,axis=0)
-
+M = 5
+iterations = [1,2,4]
 
 omegaB = 25.0
 omegaE = 4.9
 epsilon = -1
+
+x_plot_range = [-1,1]
+runs = 10
+
+omega_dt = np.logspace(x_plot_range[0],x_plot_range[1],runs)
+omega_dt = np.flip(omega_dt)
+dt = omega_dt/omegaB
+dt = np.array([0.02,0.01,0.005])
+omega_dt= dt*omegaB
 
 sim_params = {}
 species_params = {}
-case_params = {}
+ploader_params = {}
 analysis_params = {}
 data_params = {}
 
+
 sim_params['t0'] = 0
 sim_params['tEnd'] = 1
-sim_params['percentBar'] = False
-sim_params['simID'] = 'winkel_order'
+sim_params['percentBar'] = True
+sim_params['dimensions'] = 3
+sim_params['xlimits'] = [0,20]
+sim_params['ylimits'] = [0,20]
+sim_params['zlimits'] = [0,15]
+sim_params['simID'] = 'penning'
 
-species_params['mq'] = 1
 species_params['q'] = 1
 species_params['a'] = 1
-
-case_params['dimensions'] = 3
-case_params['particle_init'] = 'direct'
-case_params['pos'] = np.array([[10,0,0]])
-case_params['vel'] = np.array([[100,0,100]])
+mq = species_params['q']/species_params['a']
+ploader_params['load_type'] = 'direct'
+ploader_params['speciestoLoad'] = [0]
+ploader_params['pos'] = np.array([[10,0,0]])
+ploader_params['vel'] = np.array([[100,0,100]])
 
 H1 = epsilon*omegaE**2
 H = np.array([[H1,1,H1,1,-2*H1,1]])
-H = species_params['mq']/2 * np.diag(H[0])
+H = mq/2 * np.diag(H[0])
 
+
+analysis_params['particleIntegration'] = True
 analysis_params['M'] = M
-analysis_params['centreMass_check'] = False
-analysis_params['residual_check'] = False
-analysis_params['rhs_check'] = True
-analysis_params['fieldAnalysis'] = 'coulomb'
-analysis_params['E_type'] = 'custom'
+analysis_params['K'] = 3
+analysis_params['fieldIntegration'] = True
+analysis_params['field_type'] = 'coulomb'
+analysis_params['external_fields'] = True
+analysis_params['E_type'] = 'transform'
 analysis_params['E_transform'] = np.array([[1,0,0],[0,1,0],[0,0,-2]])
 analysis_params['E_magnitude'] = -epsilon*omegaE**2/species_params['a']
 analysis_params['B_type'] = 'uniform'
 analysis_params['B_transform'] = [0,0,1]
 analysis_params['B_magnitude'] = omegaB/species_params['a']
+analysis_params['hooks'] = ['energy_calc_penning']
+analysis_params['H'] = H
+
+analysis_params['centreMass_check'] = True
+analysis_params['residual_check'] = False
+analysis_params['rhs_check'] = True
+
 
 data_params['samplePeriod'] = 1
 data_params['write'] = True
 data_params['write_vtk'] = False
-data_params['time_plotting'] = True
+data_params['time_plotting'] = False
+data_params['tagged_particles'] = 'all'
 data_params['time_plot_vars'] = ['pos']
 data_params['trajectory_plotting'] = False
 data_params['trajectories'] = [1]
-data_params['domain_limits'] = [20,20,15]
+data_params['plot_limits'] = [20,20,15]
+
 
 plot_params = {}
 plot_params['legend.fontsize'] = 12
@@ -81,13 +95,19 @@ plot_params['axes.titlesize'] = 20
 plot_params['xtick.labelsize'] = 16
 plot_params['ytick.labelsize'] = 16
 plot_params['lines.linewidth'] = 3
-
+plot_params['axes.titlepad'] = 10
 data_params['plot_params'] = plot_params
 
 
+species_params = [species_params]
+loader_params = [ploader_params]
+
+run_times_inner = np.zeros((dt.shape[0],len(iterations)),dtype=np.float)
+run_times = []
+
 ## Analytical solution ##
-x0 = case_params['pos']
-v0 = case_params['vel']
+x0 = ploader_params['pos']
+v0 = ploader_params['vel']
 omegaTilde = sqrt(-2 * epsilon) * omegaE
 omegaPlus = 1/2 * (omegaB + sqrt(omegaB**2 + 4 * epsilon * omegaE**2))
 omegaMinus = 1/2 * (omegaB - sqrt(omegaB**2 + 4 * epsilon * omegaE**2))
@@ -132,19 +152,15 @@ for ts in range(0,tsteps):
 
 ## Numerical solution ##
 for key, value in schemes.items():
-    analysis_params['particleIntegration'] = value
+    analysis_params['particleIntegrator'] = value
     analysis_params['nodeType'] = key
     
+    j = 0
     for K in iterations:
         analysis_params['K'] = K
-        
-        tNum = []
-        xNum = []
-        yNum = []
-        zNum = []
-        dNum = []
         for i in range(0,len(dt)):
             sim_params['dt'] = dt[i]
+            Nt = floor(sim_params['tEnd']/dt[i]) +1
             
             xMod = Rplus*cos(omegaPlus*dt[i]) + Rminus*cos(omegaMinus*dt[i]) + Iplus*sin(omegaPlus*dt[i]) + Iminus*sin(omegaMinus*dt[i])
             yMod = Iplus*cos(omegaPlus*dt[i]) + Iminus*cos(omegaMinus*dt[i]) - Rplus*sin(omegaPlus*dt[i]) - Rminus*sin(omegaMinus*dt[i])
@@ -156,15 +172,14 @@ for key, value in schemes.items():
             xOne = [xMod,yMod,zMod]
             vHalf = v_half_dt
             
-            
+            sim_params['simID'] = "penning" + "_" + value + "_K" + str(K) + "_NT" + str(Nt)
             finalTs = floor(sim_params['tEnd']/dt[i])
             model = dict(simSettings=sim_params,
                          speciesSettings=species_params,
+                         pLoaderSettings=loader_params,
                          analysisSettings=analysis_params,
-                         caseSettings=case_params,
                          dataSettings=data_params)
             
-
             kppsObject = kpps(**model)
             
             
@@ -172,15 +187,17 @@ for key, value in schemes.items():
                 dHandler = kppsObject.run()
                 s_name = dHandler.controller_obj.simID
             elif simulate == False:
+                dHandler = DH()
                 s_name = sim_params['simID'] + '(' + str(sim_no) + ')'
                 if sim_no == 0:
                     s_name = sim_params['simID']
-            
-            sim, garbage = dHandler.load_sim(sim_name=s_name)
+
+            sim, garbage = dHandler.load_sim(sim_name=s_name,overwrite=True)
             rhs_evals[i] = sim.rhs_eval
             
             var_list = ['pos','energy']
-            data_dict = dHandler.load_p(var_list,sim_name=s_name)
+            data_list = dHandler.load_p(var_list,sim_name=s_name)
+            data_dict = data_list[0]
             
             tArray = data_dict['t']
             xArray = data_dict['pos'][:,0,0]
@@ -192,8 +209,13 @@ for key, value in schemes.items():
             xRel[i] = abs(xArray[-1] - xAnalyt[-1])/abs(xAnalyt[-1])
             yRel[i] = abs(yArray[-1] - yAnalyt[-1])/abs(yAnalyt[-1])
             zRel[i] = abs(zArray[-1] - zAnalyt[-1])/abs(zAnalyt[-1])
+            
+            run_times_inner[i,j] = sim.runTime
+            
             sim_no += 1
-  
+            
+        j += 1
+        
         exactEnergy = np.array(exactEnergy)
         energyError = abs(hArray[1:]-exactEnergy[1:])
         energyConvergence = energyError - energyError[0]
@@ -207,24 +229,26 @@ for key, value in schemes.items():
         
         
         ##Order Plot w/ rhs
-        fig_rhs = plt.figure(50)
+        fig_rhs = plt.figure(dHandler.figureNo+1)
         ax_rhs = fig_rhs.add_subplot(1, 1, 1)
         ax_rhs.plot(rhs_evals,xRel,label=label_order)
 
         
         ##Order Plot w/ dt
-        fig_dt = plt.figure(51)
+        fig_dt = plt.figure(dHandler.figureNo+2)
         ax_dt = fig_dt.add_subplot(1, 1, 1)
-        ax_dt.plot(dt*omegaB,xRel,label=label_order)
+        ax_dt.plot(omega_dt,xRel,label=label_order)
         
         
         ##Energy Plot
-        fig2 = plt.figure(52)
+        fig2 = plt.figure(dHandler.figureNo+3)
         ax2 = fig2.add_subplot(1, 1, 1)
         ax2.scatter(tArray[1:],hArray[1:],label=label_order)
         
         if key == 'boris':
             break
+    
+    run_times.append(run_times_inner)
         
 
 ## Order plot finish
@@ -250,7 +274,7 @@ ax_rhs.legend()
 ## Order plot finish
 ax_dt.set_xscale('log')
 #ax_dt.set_xlim(10**-3,10**-1)
-ax_dt.set_xlabel('\Delta t$')
+ax_dt.set_xlabel('$\omega_B \Delta t$')
 ax_dt.set_yscale('log')
 #ax_dt.set_ylim(10**(-7),10**1)
 ax_dt.set_ylabel('$\Delta x^{(rel)}$')
@@ -274,4 +298,6 @@ ax2.set_xlabel('$t$')
 ax2.set_ylim(0,10**4)
 ax2.set_ylabel('$\Delta E$')
 ax2.legend()
+
+print(run_times[0])
 
