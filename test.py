6f292a1b7c1b93df6d811546a05c5affedc080fd
Krissmedt <kristoffer@smedt.dk>
2019-09-06 15:29:10 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -31,7 +31,7 @@ def cell_index(pos,O,dh):
     return li
 
 def scatter_1d(pos_list,q):
-    for pii in range(0,nq):
+    for pii in range(0,pos_list.shape[0]):
         li = cell_index(pos_list[pii,2],O,dh)
 
         rpos = pos_list[pii,2] - O - li*dh
@@ -43,19 +43,30 @@ def scatter_1d(pos_list,q):
 
 outerLimits = [-3,3]
 limits = [-1,1]
-plot_ilims = [0,None]
 O = limits[0]
 dx_mag = 10
 qSum = -(limits[1]**3 - limits[0]**3)
 
 res = 200
-ppc = 1000
+ppc = 20
 
 nq = np.int(res*ppc)
-meshq = np.zeros((res+1))
-meshpos = np.linspace(limits[0],limits[1],res+1)
+
+#y_range = np.linspace(-3,0,num=np.int(nq/2))
+y_range = np.linspace(0,-1,num=np.int(nq/2))
+
+#x_range1 = np.sqrt(-y_range/3)
+#x_range1 = np.flip(x_range1,axis=0)
+#x_range2 = -np.sqrt(-y_range/3)
+#x_range = np.append(x_range2,x_range1[1:])
+
+x_range2 = np.power(-y_range,1/3)
+x_range1 = -x_range2
+x_range = np.append(x_range2,x_range1[1:])
 
 L = limits[1] - limits[0]
+dx = L/res
+
 outerL = outerLimits[1] - outerLimits[0]
 spacing = outerL/nq
 
@@ -77,24 +88,24 @@ x = [y for y in x if y != 'out']
 x0 = [y for y in x0 if y != 'out']
 xi = [y for y in xi if y != 'out']
 
+x = x_range
+
 nq = len(x)
 
 pos_spec1 = np.zeros((nq,3),dtype=np.float)
-pos_spec2 = np.zeros((nq,3),dtype=np.float)
-
 pos_spec1[:,2] = np.array(x)
-pos_spec2[:,2] = np.array(x0)
 
 q = qSum/nq
-q2 = -q
 
 dh = L/res
 
-meshq = scatter_1d(pos_spec1,q)
+meshpos = np.linspace(limits[0],limits[1]+dh,res+2)
+meshq = np.zeros((res+2))
+meshq = scatter_1d(pos_spec1[:-1],q)
 #meshq = scatter_1d(pos_spec2,q2)
 
 meshq[0] = meshq[0]*2
-meshq[-1] = meshq[-1]*2
+meshq[-2] = meshq[-2]*2
 #meshq = meshq + nq*q2/(res+1)
 meshrho = meshq/dh
 
@@ -107,8 +118,6 @@ for pii in range(1,nq):
     p_midpoints.append(x[pii]-space)
 
 
-si = plot_ilims[0]
-ei = plot_ilims[1]
 
 fig1 = plt.figure(1)
 plt.plot(p_midpoints,spacing,'bo')
@@ -118,23 +127,25 @@ plt.plot(x0,xi,'b')
 
 
 fig3, ax3 = plt.subplots()
-ax3.plot(meshpos[si:ei],meshq[si:ei],label=r'Mesh $q$')
+ax3.plot(meshpos[0:-1],meshq[0:-1],label=r'Mesh $q$')
 ax3.set_xlim([-1,1])
 #ax4.set_ylim([-0.21,0.21])
 
+real_q = -3*np.power(meshpos[0:-1],2)*dh
 ax3b = ax3.twinx()
-ax3.plot(meshpos[si:ei],-3*np.power(meshpos[si:ei],2)*dh,'r',label=r'Real $q$')
+ax3.plot(meshpos[0:-1],real_q,'r',label=r'Real $q$')
 #ax4b.set_ylim([-1,1])
 fig3.legend(loc='upper right',bbox_to_anchor=(0.85,0.83))
 
 
 fig4, ax4 = plt.subplots()
-ax4.plot(meshpos[si:ei],meshrho[si:ei],label=r'Mesh $\rho$')
+ax4.plot(meshpos[0:-1],meshrho[0:-1],label=r'Mesh $\rho$')
 ax4.set_xlim([-1,1])
 #ax4.set_ylim([-0.21,0.21])
 
+real_rho = -3*np.power(meshpos[0:-1],2)
 ax4b = ax4.twinx()
-ax4.plot(meshpos[si:ei],-3*np.power(meshpos[si:ei],2),'r',label=r'Real $\rho$')
+ax4.plot(meshpos[0:-1],real_rho,'r',label=r'Real $\rho$')
 #ax4b.set_ylim([-1,1])
 fig4.legend(loc='upper right',bbox_to_anchor=(0.85,0.83))
 
