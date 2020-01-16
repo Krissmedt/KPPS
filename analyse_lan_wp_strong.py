5e6d69b3310b5a9d4bec9f3b46a5781ba1410b9f
Krissmedt <kristoffer@smedt.dk>
2020-01-16 10:54:18 +0000

    Strong damping analysis tailored

--- analyse_lan_wp_strong.py
+++ analyse_lan_wp_strong.py
@@ -34,11 +34,11 @@ def find_peaks(peak_intervals,EL2,dt,samplePeriod):
     return peaks
 
 analyse = True
-plot = False
-snapPlot = True
-compare_reference = False
+plot = True
+snapPlot = False
+compare_reference = True
 
-peak_intervals = [[0,2],[2,4],[4,6],[6,8],[8,11]]
+peak_intervals = [[0,2],[2,4],[4,6],[6,8],[8,10]]
 peak_intervals2 = [[20,22.5],[22.5,25],[25,27.5],[27.5,30],[30,32.5],[32.5,35],[35,37.5],[37.5,40]]
 #peak_intervals = [[2,4],[4,6],[6,8]]
 #peak_intervals = [[0,2],[2,4],[4,6]]
@@ -48,18 +48,21 @@ fit1_stop = peak_intervals[-1][-1]
 fit2_start = peak_intervals2[0][0]
 fit2_stop = peak_intervals2[-1][-1]
 
-compare_time = 1
+analysis_times = [1,2,3,4,5,6,7,8,9,10]
+compare_times = [4]
+
 
 fig_type = ''
-data_root = "../data_landau/"
+data_root = "../data_landau_strong/"
 sims = {}
 
 #sims['lan_TE10_a0.05_boris_staggered_NZ10_NQ20000_NT'] = [20,100,200]
 #sims['lan_TE10_a0.5_boris_staggered_NZ1000_NQ20000_NT'] = [10,20,30,40,50,100,500,1000]
 #sims['lan_TE10_strong_boris_SDC_NZ10000_NQ200000_NT'] = [5000]
-sims['lan_TE50_K0.5_boris_staggered_NZ256_NQ163000_NT'] = [500]
+sims['lan_TE10_a0.5_boris_staggered_NZ1000_NQ20000_NT'] = [10,20,30,40,50,100,500]
+sims['lan_TE10_a0.5_boris_SDC_NZ1000_NQ20000_NT'] = [10,20,30,40,50,100]
 
-comp_run = 'lan_TE50_K0.5_boris_staggered_NZ256_NQ163000_NT500'
+comp_run = 'lan_TE10_a0.5_boris_staggered_NZ1000_NQ20000_NT1000'
 
 
 ################################ Linear analysis ##############################
@@ -106,9 +109,7 @@ if analyse == True:
         DH_comp = dataHandler2(**data_params)
         comp_sim, comp_sim_name = DH_comp.load_sim(sim_name=comp_run,overwrite=True)
         mData_comp = DH_comp.load_m(['phi','E','dz'],sim_name=comp_sim_name)
-        
-        cvalue = np.int(compare_time/(comp_sim.dt*DH_comp.samplePeriod))
-        
+
         tArray_comp = mData_comp['t']
         dz_comp = mData_comp['dz'][0] 
 
@@ -121,8 +122,6 @@ if analyse == True:
         EL2 = np.sum(E*E,axis=1)
         EL2_comp = np.sqrt(EL2)
         
-        EL2_ref = EL2_comp[cvalue]
-        
         comp_peaks = find_peaks(peak_intervals,EL2_comp,comp_sim.dt,DH_comp.samplePeriod)
         
         comp_fit = np.polyfit(tArray_comp[comp_peaks],
@@ -133,12 +132,11 @@ if analyse == True:
         dts = []
         Nts = []
         rhs_evals = []
-        avg_slopes = []
-        avg_errors = []
-        avg_errors_nonlinear = []
+        gamma1_errors = []
+        gamma2_errors = []
         E_errors = []
         
-        filename = key[:-3] + "_wp_"  + str(compare_time)+ "s"  ".h5"
+        filename = key[:-3] + "_wp_strong.h5"
         filenames.append(filename)
         file = h5.File(data_root+filename,'w')
         grp = file.create_group('fields')
@@ -158,8 +156,11 @@ if analyse == True:
                 NC = np.int(fit2_start/(sim.dt*DH.samplePeriod))
                 ND = np.int(fit2_stop/(sim.dt*DH.samplePeriod))
                 
-                cvalue = np.int(compare_time/(sim.dt*DH.samplePeriod))
-    
+                analysis_ts = []
+                for time in analysis_times:
+                    analysis_ts.append(np.int(time/(sim.dt*DH.samplePeriod)))
+                    
+
                 mData_dict = DH.load_m(['phi','E','rho','PE_sum','zres','dz'],sim_name=sim_name)
                 tArray = mData_dict['t']
     
@@ -171,40 +172,44 @@ if analyse == True:
                 
                 EL2 = np.sum(E*E,axis=1)
                 EL2 = np.sqrt(EL2)  
+
+                try:
+                    peaks = find_peaks(peak_intervals,EL2,sim.dt,DH.samplePeriod)
+                    c1 = EL2[peaks[0]]*1.05
+                    E_fit = np.polyfit(tArray[peaks],np.log(EL2[peaks]),1)
+                    E_fit = np.around(E_fit,decimals=6)
+                    E_fit_line = c1*np.exp(E_fit[0]*tArray[NA:NB])
+                    gamma_line = c1*np.exp(gamma*tArray[NA:NB])
+                    lit_line = c1*np.exp(gamma_lit1*tArray[NA:NB])
+                    error_gamma1 = abs(gamma_lit1 - E_fit[0])/abs(gamma_lit1)
+                    gamma1_errors.append(error_gamma1)
+                except Exception:
+                    pass
                 
-                EL2_sim = EL2[cvalue]
+                try:
+                    peaks2 = find_peaks(peak_intervals2,EL2,sim.dt,DH.samplePeriod)
+                    c2 = EL2[peaks2[0]]*0.17
+                    E_fit2 = np.polyfit(tArray[peaks2],np.log(EL2[peaks2]),1)
+                    E_fit2 = np.around(E_fit2,decimals=6)
+                    E_fit_line2 = c2*np.exp(E_fit2[0]*tArray[NC:ND])
+                    lit_line2 = c2*0.87*np.exp(gamma_lit2*tArray[NC:ND])
+                    error_gamma2 = abs(gamma_lit2 - E_fit2[0])/abs(gamma_lit2)
+                    gamma2_errors.append(error_gamma2)
+                except Exception:
+                    pass
                 
-                peaks = find_peaks(peak_intervals,EL2,sim.dt,DH.samplePeriod)
-                peaks2 = find_peaks(peak_intervals2,EL2,sim.dt,DH.samplePeriod)
-                c1 = EL2[peaks[0]]*1.05
-                c2 = EL2[peaks2[0]]*0.17
-                
-                E_fit = np.polyfit(tArray[peaks],np.log(EL2[peaks]),1)
-                E_fit = np.around(E_fit,decimals=6)
-                E_fit_line = c1*np.exp(E_fit[0]*tArray[NA:NB])
-                
-                E_fit2 = np.polyfit(tArray[peaks2],np.log(EL2[peaks2]),1)
-                E_fit2 = np.around(E_fit2,decimals=6)
-                E_fit_line2 = c2*np.exp(E_fit2[0]*tArray[NC:ND])
-                
-                gamma_line = c1*np.exp(gamma*tArray[NA:NB])
-                lit_line = c1*np.exp(gamma_lit1*tArray[NA:NB])
-                lit_line2 = c2*0.87*np.exp(gamma_lit2*tArray[NC:ND])
-                
-                error_linear = abs(gamma_lit1 - E_fit[0])/abs(gamma_lit1)
-                error_linear2 = abs(gamma_lit2 - E_fit2[0])/abs(gamma_lit2)
-        
                 
                 dts.append(sim.dt)
                 Nts.append(sim.tSteps)
                 rhs_evals.append(sim.rhs_eval)
-                avg_slopes.append(E_fit[0])
-                avg_errors.append(error_linear)
-                
+
                 if compare_reference == True:
-                    E_error = abs(EL2_ref-EL2_sim)/EL2_ref
-                    E_errors.append(E_error)
-                
+                    skip = (sim.dt*DH.samplePeriod)/(comp_sim.dt*DH_comp.samplePeriod)
+                    skip_int = np.int(skip)
+                    E_error = np.abs(EL2_comp[::skip_int]-EL2)/np.abs(EL2_comp[::skip_int])
+                    E_errors.append(E_error[analysis_ts])
+
+                    
                 if snapPlot == True:
                     fig = plt.figure(DH.figureNo+sim_no)
                     E_ax = fig.add_subplot(1,1,1)
@@ -212,30 +217,33 @@ if analyse == True:
                     
                     E_ax.scatter(tArray[peaks],EL2[peaks])
                     E_ax.plot(tArray[NA:NB],E_fit_line,'orange',label="Fitted $\gamma$")
-#                    E_ax.plot(tArray[NA:NB],gamma_line,'red',label="$Analyt$")
                     E_ax.plot(tArray[NA:NB],lit_line,'green',label="Literature $\gamma$")
-#
-                    E_ax.scatter(tArray[peaks2],EL2[peaks2])
-                    E_ax.plot(tArray[NC:ND],E_fit_line2,'orange')
-#                    E_ax.plot(tArray[NC:ND],gamma_line,'red',label="$Analyt$")
-                    E_ax.plot(tArray[NC:ND],lit_line2,'green')
                     
                     E_text1 = E_ax.text(.05,0,'',transform=E_ax.transAxes,verticalalignment='bottom',fontsize=14)
                     text1 = (r'$\gamma_{fit}$ = ' + str(E_fit[0]) + ' vs. ' + r'$\gamma_{lit}$ = ' + str(gamma_lit1))
                     E_text1.set_text(text1)
                     
-                    E_text2 = E_ax.text(.5,0,'',transform=E_ax.transAxes,verticalalignment='bottom',fontsize=14)
-                    text2 = (r'$\gamma_{fit}$ = ' + str(E_fit2[0]) + ' vs. ' + r'$\gamma_{lit}$ = ' + str(gamma_lit2))
-                    E_text2.set_text(text2)
+                    try:
+                        E_ax.scatter(tArray[peaks2],EL2[peaks2])
+                        E_ax.plot(tArray[NA:NB],E_fit_line2,'orange',label="Fitted $\gamma$")
+                        E_ax.plot(tArray[NA:NB],lit_line2,'green',label="Literature $\gamma$")
+                        
+                        E_text2 = E_ax.text(.05,0,'',transform=E_ax.transAxes,verticalalignment='bottom',fontsize=14)
+                        text2 = (r'$\gamma_{fit}$ = ' + str(E_fit2[0]) + ' vs. ' + r'$\gamma_{lit}$ = ' + str(gamma_lit2))
+                        E_text2.set_text(text2)
+                    except Exception:
+                        pass
+                        
                     
                     E_ax.set_xlabel('$t$')
                     E_ax.set_ylabel(r'$||E||_{L2}$')
                     E_ax.set_yscale('log')
                     #E_ax.set_ylim([10**-7,10**-1])
-                    E_ax.set_title('Non-linear Landau damping, a=0.5, NQ=' + str(nq) + ', NZ=' + str(mData_dict['zres'][0]) 
+                    E_ax.set_title('Linear Landau damping, a=0.05, NQ=' + str(nq) + ', NZ=' + str(mData_dict['zres'][0]) 
                                     + ', NT=' + str(sim.tSteps)) 
                     E_ax.legend()
                     fig.savefig(data_root + sim_name + '_EL2.png', dpi=150, facecolor='w', edgecolor='w',orientation='portrait')
+                    
                     
                     
             
@@ -250,7 +258,8 @@ if analyse == True:
             grp.create_dataset('dts',data=dts)
             grp.create_dataset('Nts',data=Nts)
             grp.create_dataset('rhs_evals',data=rhs_evals)
-            grp.create_dataset('errors',data=avg_errors)
+            grp.create_dataset('gamma1_errors',data=gamma1_errors)
+            grp.create_dataset('gamma2_errors',data=gamma2_errors)
             grp.create_dataset('energy',data=UE)
             grp.create_dataset('energy_reference',data=UE_comp)
             grp.create_dataset('E_errors',data=E_errors)
@@ -264,7 +273,7 @@ if analyse == True:
 if plot == True:
     if len(filenames) == 0:
         for key, value in sims.items():
-            filename = key[:-3] + "_wp_"  + str(max_time)+ "s"  ".h5"
+            filename = key[:-3] + "_wp_"  + "_strong.h5"
             filenames.append(filename)
             
 
@@ -273,8 +282,10 @@ if plot == True:
         file = h5.File(data_root+filename,'r')
         dts = file["fields/dts"][:]
         rhs_evals = file["fields/rhs_evals"][:]
-        avg_errors = file["fields/errors"][:]
+        gamma1_errors = file["fields/gamma1_errors"][:]
+        gamma2_errors = file["fields/gamma2_errors"][:]
         E_errors = file["fields/E_errors"][:]
+        E_errors = np.array(E_errors)
 
         if file.attrs["integrator"] == "boris_staggered":
             label = "Boris Staggered" + ", Nz=" + file.attrs["res"]
@@ -289,23 +300,27 @@ if plot == True:
             ##Order Plot w/ rhs
             fig_nl_rhs = plt.figure(10)
             ax_nl_rhs = fig_nl_rhs.add_subplot(1, 1, 1)
-            ax_nl_rhs.plot(rhs_evals,E_errors,label=label)
+            for time in compare_times:
+                label_line = label + ', ' + str(time) + 's'
+                ax_nl_rhs.plot(rhs_evals,E_errors[:,time-1],label=label_line)
                 
             ##Order Plot w/ dt
             fig_nl_dt = plt.figure(11)
             ax_nl_dt = fig_nl_dt.add_subplot(1, 1, 1)
-            ax_nl_dt.plot(dts,E_errors,label=label)
+            for time in compare_times:
+                label_line = label + ', ' + str(time) + 's'
+                ax_nl_dt.plot(dts,E_errors[:,time-1],label=label_line)
             
-        print(avg_errors)
+
         ##Order Plot w/ rhs
         fig_rhs = plt.figure(12)
         ax_rhs = fig_rhs.add_subplot(1, 1, 1)
-        ax_rhs.plot(rhs_evals,avg_errors,label=label)
+        ax_rhs.plot(rhs_evals,gamma1_errors,label=label)
             
         ##Order Plot w/ dt
         fig_dt = plt.figure(13)
         ax_dt = fig_dt.add_subplot(1, 1, 1)
-        ax_dt.plot(dts,avg_errors,label=label)
+        ax_dt.plot(dts,gamma1_errors,label=label)
         file.close()
         
     ax_list = []  
@@ -361,7 +376,7 @@ if plot == True:
             ax.set_ylim(10**(-5),10)
             ax.set_ylabel(r'E L2 Error $\Delta (\sum \frac{E_i^2}{2} \Delta x)$')
             
-            
+            ax.set_title('Convergence vs. Ref Solution at '+ str(compare_times) + 's')
             xRange = ax.get_xlim()
             yRange = ax.get_ylim()
             
@@ -371,5 +386,5 @@ if plot == True:
                         ls='dashed',c='0.75',label='4th Order')
             
             ax.legend(loc = 'best')
-            fig_nl_rhs.savefig(data_root + 'tsi_growth_'+ fig_type +"_"+ str(max_time) + 's_rhs.png', dpi=150, facecolor='w', edgecolor='w',orientation='portrait')
-            fig_nl_dt.savefig(data_root + 'tsi_growth_' + fig_type +"_"+ str(max_time) + 's_dt.png', dpi=150, facecolor='w', edgecolor='w',orientation='portrait')
+            fig_nl_rhs.savefig(data_root + 'landau_weak_'+ fig_type +"_"+ str(compare_times) + 's_rhs.png', dpi=150, facecolor='w', edgecolor='w',orientation='portrait')
+            fig_nl_dt.savefig(data_root + 'landau_weak_' + fig_type +"_"+ str(compare_times) + 's_dt.png', dpi=150, facecolor='w', edgecolor='w',orientation='portrait')
